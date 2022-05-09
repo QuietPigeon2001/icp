218944dd7d2437d0347259f40ae41a64de0a1a61
pigeon <91387291+QuietPigeon2001@users.noreply.github.com>
2022-05-09 09:46:51 +0900

    Update and rename year1/report2/nested.f90 to year1/nested.f90

--- year1/nested.f90
+++ year1/nested.f90
@@ -1,17 +1,23 @@
 program summation
 implicit none
+
 integer:: p
 real(8):: sum, x
+
 write(*,*) ("Input a finite number of radicals: ")
 read(*,*) p
 x = (1+p)**0.5
-    if (p <= 2) then 
+
+if (p <= 2) then 
     x = (1+(p-1)*p)**0.5
-    end if
-    do while (p > 2)
+end if
+
+do while (p > 2)
     sum = (1+(p-1)*x)**0.5
     x = sum
     p = p - 1
-    end do
-    write(*,*) x
-    end program summation
+end do
+
+write(*,*) x
+
+end program summation
